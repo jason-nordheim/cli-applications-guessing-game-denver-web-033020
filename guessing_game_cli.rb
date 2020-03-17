a4ab766037f1bad2b8f8a33bafbd581997bd2c3b
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-17 14:56:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,15 @@
 # Code your solution here!
 
 def run_guessing_game()
-  # generate random number 
+  # generate random number
   num = rand(1...6)
   p num
 
-  # prompt the user for input 
-  
+  # prompt the user for input
+
 end
+
+def prompt_for_num()
+end 
 
 run_guessing_game()
